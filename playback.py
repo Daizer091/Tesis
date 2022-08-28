6620a88f8c56d1e1d0c4d293e4eb184c70cdd60e
Daizer <falcon127@outlook.es>
2022-08-28 16:38:31 -0500

    cambio de nombre de archivos, organizacion de archivo, codigo de generador

--- playback.py
+++
@@ -1,9 +0,0 @@
-import sounddevice as sd
-import soundfile as sf
-
-fs = 44100
-array, smp_rt = sf.read('./prueba.wav', dtype='float32')
-# myarray = read('./sounds/recording0.wav', True)
-# myarray = numpy.array(myarray)
-sd.play(array, smp_rt)
-sd.wait()
